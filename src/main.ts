228f22362ce30f45b68c7099919368401ee9d630
tinmn9088 <tinman.8.9@yandex.ru>
2022-10-21 11:19:41 +0300

    feat: window shows if ready

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,15 @@ const createWindow = () => {
     width: 400,
     height: 550,
     resizable: false,
-    icon: 'assets/favicon.png'
+    icon: 'assets/favicon.png',
+    show: false
   });
 
   // win.webContents.openDevTools();
   
   win.setMenu(null);
   win.loadFile(path.join(__dirname, "/index.html"));
+  win.on("ready-to-show", () => win.show());
 }
 
 app.on("ready", () => {
